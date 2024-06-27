4fa85fa0817df2b24f16dbddcbb5a368e3221584
GHarrDev <gharris@icstars.org>
2024-06-26 20:44:46 -0500

    updated scripts in .json file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
+import Dashboard from "./scenes/dashboard/index";
+import ReporterDash from "./scenes/dashboard/reporter";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
@@ -30,6 +31,7 @@ function App() {
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="reporter" element={<ReporterDash />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
